69376716c0df976f1bc08c95c31c52941af87cc0
Nikka Hassan <nikka.hassan@gmail.com>
2019-01-14 16:14:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object 
 }
 function deleteFromObjectByKey(object, key) {
-  var newObject = Object.assign({}, object)
+  var newObject = Object.assign({}, object, key)
 }
